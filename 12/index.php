25b995952de5388bad47a0868c3da140138d6ad4
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-05-28 23:00:14 +0530

    Update index.php

--- 12/index.php
+++ 12/index.php
@@ -7,37 +7,23 @@
 
     <style>
         .container {
-            margin: 50px auto;
-            width: 400px;
-            padding: 30px;
-            background-color: #f5f5f5;
-            border-radius: 10px;
-            box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
-        }
+    margin: 50px auto;
+    width: 400px;
+    padding: 30px;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
+     }
 
-        input {
-            font-size: 16px;
-            padding: 10px;
-            margin: 5px;
-            border-radius: 5px;
-            width: calc(100% - 20px);
-            border: 1px solid #ccc;
-        }
+    input{
+    font-size: 12px;
+    padding: 10px;
+    margin: 5px;
+    border-radius: 2px;
+    width: 70%;
+     }
 
-        button {
-            font-size: 16px;
-            background-color: rgb(204, 222, 216);
-            text-align: center;
-            padding: 10px;
-            border-radius: 5px;
-            border: none;
-            cursor: pointer;
-            width: 100%;
-        }
 
-        h2 {
-            text-align: center;
-        }
     </style>
 
 </head>
