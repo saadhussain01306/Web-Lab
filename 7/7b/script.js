49922b9061b23dc41b03074e601e0a5ffdf5547c
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-07-26 01:34:06 +0530

    Update script.js

--- 7/7b/script.js
+++ 7/7b/script.js
@@ -1,46 +1,38 @@
-function validate(){
+function validate() {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let mobile = document.getElementById("mobile").value;
     let gender = document.getElementById("gender").value;
     let color = document.getElementById("color").value;
 
-    if(name===""){
+    if(name === "") {
         alert("Please enter your name");
         return;
     }
-    if(email==="" || !email.includes(".") || !email.includes("@")){
+    if(email === "" || !email.includes(".") || !email.includes("@")) {
         alert("Please enter a valid Email");
         return;
     }
-    if(mobile==="" || mobile.length!==10 || isNaN(mobile)){
+    if(mobile === "" || mobile.length !== 10 || isNaN(mobile)) {
         alert("Please Enter a valid Mobile Number");
         return;
     }
-    if(gender===""){
+    if(gender === "") {
         alert("Please enter your Gender");
         return;
     }
-    if(color===""){
+    if(color === "") {
         alert("Please Enter Color");
         return;
     }
 
-    // object with the entered infotrmation
-    let display = {
-        Name: name,
-        Email: email,
-        Mobile: mobile,
-        Gender: gender,
-        Color: color
-    };
+    
+    let output = "Name: " + name + "\n" +
+                 "Email: " + email + "\n" +
+                 "Mobile: " + mobile + "\n" +
+                 "Gender: " + gender + "\n" +
+                 "Color: " + color + "\n";
 
-    // append the infortmation to the string
-    let output = "";
-    for (let key in display) {
-        output += key + ": " + display[key] + "\n";
-    }
-
-    // alert the information
+    // Alert the information
     alert(output);
 }
